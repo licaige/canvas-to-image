db04258920f286e2b9c4f607c4377626d5240332
liqi <593481865@qq.com>
2021-01-06 16:48:38 +0800

    戳石化

---
+++ index/index.js
@@ -0,0 +1,28 @@
+const { wxml, style } = require('./demo.js')
+Page({
+  data: {
+    src: ''
+  },
+  onLoad() {
+    this.widget = this.selectComponent('.widget')
+  },
+  renderToCanvas() {
+    const p1 = this.widget.renderToCanvas({ wxml, style })
+    p1.then((res) => {
+      console.log('container res.layoutBox', res.layoutBox)
+      console.log('container res', res)
+      this.container = res
+    })
+  },
+  //导出图片
+  extraImage() {
+    const p2 = this.widget.canvasToTempFilePath()
+    p2.then(res => {
+      this.setData({
+        src: res.tempFilePath,
+        width: this.container.layoutBox.width,
+        height: this.container.layoutBox.height
+      })
+    })
+  }
+})
