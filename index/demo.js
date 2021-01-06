68f1828a20a346ff701a230cd5d4d6fde0a63089
liqi <593481865@qq.com>
2021-01-06 17:31:32 +0800

    升级版本

--- index/demo.js
+++ index/demo.js
@@ -1,50 +1,29 @@
 const wxml = `
-<view class="container" >
-  <view class="item-box red">
-  </view>
-  <view class="item-box green" >
-    <text class="text">yeah!</text>
-  </view>
-  <view class="item-box blue">
-      <image class="img" src="https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3582589792,4046843010&fm=26&gp=0.jpg"></image>
-  </view>
+<view class="outline">
+    <image class="outlineImg" src="https://applet.lanlinker.com/lanlinker-applets/image/0d8001282e424ab8a070006dec295862_sharebg.png"></image>
+    <image class="lineImg" src="https://applet.lanlinker.com/lanlinker-applets/image/0085f45f92e4431ba1b250eab86a9858_xiaochengxun.jpg"></image>
 </view>
 `
-
 const style = {
-    container: {
-        width: 300,
-        height: 200,
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        backgroundColor: '#ccc',
-        alignItems: 'center',
-    },
-    itemBox: {
-        width: 80,
-        height: 60,
-    },
-    red: {
-        backgroundColor: '#ff0000'
-    },
-    green: {
-        backgroundColor: '#00ff00'
-    },
-    blue: {
-        backgroundColor: '#0000ff',
-        alignItems: 'center',
+    outline: {
+        display: 'flex',
         justifyContent: 'center',
+        alignItems: 'center',
+        width: 576,
+        height: 768,
+        position: 'relative',
     },
-    text: {
-        width: 80,
-        height: 60,
-        textAlign: 'center',
-        verticalAlign: 'middle',
+    outlineImg: {
+        display: 'block',
+        width: 576,
+        height: 768,
     },
-    img: {
-        width: 40,
-        height: 40,
-        borderRadius: 20,
+    lineImg: {
+        position: 'absolute',
+        width: 260,
+        height: 260,
+        left: 137,
+        bottom: 160,
     }
 }
 
--- index/index.js
+++ index/index.js
@@ -7,10 +7,12 @@ Page({
     },
     onLoad() {
         this.widget = this.selectComponent('.widget')
+        console.log('this.widget 李琦',this.widget)
     },
-    //渲染到canvas
+    //渲染到canvas,生成图片
     renderToCanvas() {
         const p1 = this.widget.renderToCanvas({wxml, style})
+        console.log('p1 李琦',p1)
         p1.then((res) => {
             console.log('container res.layoutBox', res.layoutBox)
             console.log('container res', res)
@@ -20,6 +22,7 @@ Page({
     //导出图片
     extraImage() {
         const p2 = this.widget.canvasToTempFilePath()
+        console.log('p2 李琦',p2)
         p2.then(res => {
             this.setData({
                 src: res.tempFilePath,
